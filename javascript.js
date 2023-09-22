bceb3803eaf3fd163b1cd4f7e77eecc886a031e8
MuskanMandal15 <mandalmuskan12@gmail.com>
2023-09-22 21:14:34 +0530

    add new lines

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
-alert('hello world');
+// alert('hello world'); // alert message
+
+let userName = "muskan"
+ userName = "harshita"
+ console.log(userName); // output - harshita
+
+ var person = "boy";
+ var person = "girl"
+ console.log(person) // output - girl
+  
+ const human = "lady"
+ console.log(human) // output - lady
+
+// ============================================================
+ let currentResult = 0;
+
+ currentResult = (currentResult + 10) * 3 / 2 - 1 ;
+
+ outputResult(currentResult, "")
