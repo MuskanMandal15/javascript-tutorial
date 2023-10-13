d9d4a375d80af2ddfddd0367b778e59fa539880a
muskanmandal <mandalmuskan12gmail.com>
2023-10-13 14:42:57 +0530

    added new lines in function.js

---
+++ function-ques.js
@@ -0,0 +1,123 @@
+// Problem 1:
+//Create a function that calculates the area of a rectangle. The rectangle has a length of 5
+//units and a width of 3 units. Write a JavaScript function to find the area of this rectangle.
+
+// solution 1.
+
+function areaOfRec(length,width){
+    return length * width;
+  }
+  console.log(areaOfRec(5,3));
+  
+  // ===================================================================
+  
+  // Problem 2:
+  // Find the error in the given code
+  // function addNumbers(a, b) {
+  // return a + b; }
+  // var result = addNumbers(5, 7);
+  // console.log("The result is: " result);
+  
+  // // solution 2 - we have to add comma(,) before result
+  
+  // ================================================================
+  
+  // problem 3 where do you declare function in js?
+  
+  // solution 3 - option 1 (function myFunction())
+  
+  // ========================================================
+  
+  // Problem 4:
+  // What is the purpose of parameters in a JavaScript function?
+  // solution 4 - option 3 (To accept input values when the function is called)
+  
+  // =========================================================
+  
+  // Problem 5:
+  // What does the return statement do in a JavaScript function?
+  
+  // solution 5 - option 3 (It stops the execution of the function and returns a value to the caller)
+  
+  // ================================================================
+  
+  // Problem 6:
+  // What is the difference between function declarations and function expressions in
+  // JavaScript?
+  
+  // solution 6 - option 2 (function declarations are hoisted, while function expressions are not)
+  
+  // =======================================================================
+  
+  // Problem 7:
+  // You have an array of test scores, and you want to calculate the average score. Write a
+  // JavaScript function to calculate and return the average of the scores.
+  
+  // solution 7 
+  
+  const testScores = [85, 90, 78, 92, 88];
+  
+  function calcAvreage(num){
+      let total = 0;
+      for (let value of num)
+       total += value;
+       return total / 5;
+  }
+  console.log(calcAvreage(testScores));
+  
+  // ======================================================================
+  
+  // problem 8: 
+  // You have a list of student scores, and you want to assign letter grades based on the scores.
+  // Write a JavaScript function that takes a score as input and returns the corresponding letter
+  // grade.
+  
+  const studentScore = 81;
+  
+  function gradeCard(num1){
+   if(num1>=90){
+      return "A";
+   }else if(num1>=80 && num1<=89){
+      return "B";
+   }else if(num1>=70 && num1<=79){
+      return "C";
+   }else if(num1>=60 && num1<=69){
+      return "D";
+   }else{
+     return "F"
+      
+   }
+  }
+  console.log(gradeCard(studentScore));
+  
+  
+  // ==================================================================
+  // Problem 9:
+  // Find the error in the given code
+  
+  // solution 9 - bracket missing on line number 9
+  
+  // ============================================================
+  
+  // problem 10: 
+  // How do you call a javascript function?
+  
+  // solution 10 - option 3 (by using the function's name followed by parentheses, like myFunction ())
+  
+  // =============================================================================================
+  
+  //DEFAULT PARAMETER
+  
+  function tableOfTwo(a=2){
+     for(let i=1;i<=10;i++){
+        console.log(`${a}*${i}=${a*i}`);
+     }
+  
+  }
+  tableOfTwo(5);
+  // if we already set parameter in parameter that the function will take default parameter, no matter if we already set arguments.
+  
+  // ========================================================================================
+  
+  
+  
--- function.js
+++ function.js
@@ -84,122 +84,73 @@ function greet(name, lastName) {
  
  // console.log(sum(1, 2, 3, 4, 5, 6));
 // =======================================================================
-//  Qestions of Functions
+// getters & Setters
 
-// Problem 1:
-//Create a function that calculates the area of a rectangle. The rectangle has a length of 5
-//units and a width of 3 units. Write a JavaScript function to find the area of this rectangle.
+// const person = {
+//     firstName : 'Harshita',
+//     lastName : 'Mandal',
+//     get fullName(){
+//         return `${person.firstName} ${person.lastName}`
 
-// solution 1.
+//     },
 
-function areaOfRec(length,width){
-  return length * width;
-}
-console.log(areaOfRec(5,3));
+//     set fullName(value){
+//    const parts = value.split(' ');
+//    this.firstName = parts [0];
+//    this.lastName = parts[1];
+//     }
+// };
 
-// ===================================================================
+// person.fullName = "Muskan Mandal";
 
-// Problem 2:
-// Find the error in the given code
-// function addNumbers(a, b) {
-// return a + b; }
-// var result = addNumbers(5, 7);
-// console.log("The result is: " result);
-
-// // solution 2 - we have to add comma(,) before result
-
-// ================================================================
-
-// problem 3 where do you declare function in js?
-
-// solution 3 - option 1 (function myFunction())
-
-// ========================================================
-
-// Problem 4:
-// What is the purpose of parameters in a JavaScript function?
-// solution 4 - option 3 (To accept input values when the function is called)
-
-// =========================================================
-
-// Problem 5:
-// What does the return statement do in a JavaScript function?
-
-// solution 5 - option 3 (It stops the execution of the function and returns a value to the caller)
-
-// ================================================================
-
-// Problem 6:
-// What is the difference between function declarations and function expressions in
-// JavaScript?
-
-// solution 6 - option 2 (function declarations are hoisted, while function expressions are not)
-
-// =======================================================================
-
-// Problem 7:
-// You have an array of test scores, and you want to calculate the average score. Write a
-// JavaScript function to calculate and return the average of the scores.
-
-// solution 7 
-
-const testScores = [85, 90, 78, 92, 88];
-
-function calcAvreage(num){
-    let total = 0;
-    for (let value of num)
-     total += value;
-     return total / 5;
-}
-console.log(calcAvreage(testScores));
-
-// ======================================================================
-
-// problem 8: 
-// You have a list of student scores, and you want to assign letter grades based on the scores.
-// Write a JavaScript function that takes a score as input and returns the corresponding letter
-// grade.
-
-const studentScore = 81;
-
-function gradeCard(num1){
- if(num1>=90){
-    return "A";
- }else if(num1>=80 && num1<=89){
-    return "B";
- }else if(num1>=70 && num1<=79){
-    return "C";
- }else if(num1>=60 && num1<=69){
-    return "D";
- }else{
-   return "F"
-    
- }
-}
-console.log(gradeCard(studentScore));
+// person.fullName = '';
 
 
-// ==================================================================
-// Problem 9:
-// Find the error in the given code
+// console.log(person);
 
-// solution 9 - bracket missing on line number 9
+//Getters : acess properties
+//Setters : Change (mutate) properties
 
-// ============================================================
 
-// problem 10: 
-// How do you call a javascript function?
+// Try & Catch
 
-// solution 10 - option 3 (by using the function's name followed by parentheses, like myFunction ())
+// try{
+//     // Code that may throw error
+// } catch (error) {
+//     //code to handle error
+// }
 
-// =============================================================================================
+// const person = {
+//     firstName : 'Harshita',
+//     lastName : 'Mandal',
+//     set fullName(value){
+//         if (typeof value !== 'string')
+//         throw new Error('Value is not a string');
 
-//DEFAULT PARAMETER
 
-function tableOfTwo(a=2){
-   for(let i=1;i<=10;i++){
-      console.log(`${a}*${i}=${a*i}`);
-   }
 
-}
-(tableOfTwo(5))
+//    const parts = value.split(' ');
+//    if (parts.length !==2)
+//    throw new Error('Enter a first name and last name')
+//    this.firstName = parts [0];
+//    this.lastName = parts[1];
+//     }
+// };
+
+
+// try{
+//     person.fullName = 'Prabhat Kumar';
+// } catch(error){
+//     alert(error)
+// }
+
+// console.log(person);
+
+
+
+// try{
+//     // Code that may throw error
+// } catch (error) {
+//     //code to handle error
+// }
+
