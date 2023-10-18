d6277be02afeb7bd87eb8e7b87d85ed74c4c7321
muskanmandal <mandalmuskan12gmail.com>
2023-10-18 19:43:12 +0530

    addd some line of code in arrays.js

--- arrays.js
+++ arrays.js
@@ -1,2 +1,11 @@
 let fruits = ["apple","mango","banana","grapes"];
 console.log(fruits);
+
+let numbers = new Array(10,22,3,4,55);
+console.log(numbers);
+
+let person = Array("muskan","female");
+console.log(person);
+
+let color = Array.of("pink","orange");
+console.log(color);
