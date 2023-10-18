25bedccec2b026ff846dc29a0baf6c517a078e29
muskanmandal <mandalmuskan12gmail.com>
2023-10-18 19:54:59 +0530

    code in arrays.js

--- arrays.js
+++ arrays.js
@@ -1,11 +1,29 @@
+// Types of arrays
+// type:1
 let fruits = ["apple","mango","banana","grapes"];
 console.log(fruits);
 
-let numbers = new Array(10,22,3,4,55);
+// type:2
+let numbers = new Array(10); // now it will take 10 as length of the array bcz we've gave it single value
 console.log(numbers);
 
+let numbers2 = new Array(2,3,4,6,7); // it will take these as values of the array
+console.log(numbers2);
+
+// type:3
 let person = Array("muskan","female");
 console.log(person);
 
+// type:4
 let color = Array.of("pink","orange");
 console.log(color);
+
+
+
+
+
+
+
+// ====================================================================================
+// TEXT
+//we can store different types of data, whether it is reference or primitive
